7f2ca7c98209e74bd5874006684c26920f795bfc
xuyufei <yuranfeng@126.com>
2024-04-09 19:41:24 +0800

    feat: 03

---
+++ 03/meaning.py
@@ -0,0 +1,12 @@
+meaning = 4
+print('')
+
+if meaning > 10:
+    print('Right on!')
+else:
+    print('Not today!')
+
+
+# Ternary Operator
+meaning = 12
+print('Right on!') if meaning > 10 else print('Not today')
