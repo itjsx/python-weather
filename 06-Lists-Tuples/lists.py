a1c5f8f2af90477f3df6f736af94a053decb8ff4
xujiaheng <xujiaheng@sdhxgk.com>
2024-04-10 14:00:08 +0800

    feat: 06 Lists&Tuples

---
+++ 06-Lists-Tuples/lists.py
@@ -0,0 +1,109 @@
+users = ["Dave", "Carrie", "Mal"]
+
+data = ["Dave", 43, True]
+
+emptyList = []
+
+print("Dave" in users)
+
+print(users[0])
+print(users[-2])
+
+print(users.index("Carrie"))
+print(users[0:2])
+print(users[1:])
+print(users[-3:-1])
+print(len(data))
+
+users.append("Elsa")
+print(users)
+
+users += ["Jason"]
+print(users)
+
+users.extend(["Robert", "Jimmy"])
+print(users)
+
+# users.extend(data)
+# print(users)
+
+users.insert(0, "Bob")
+print(users)
+
+users[2:2] = ["Eddie", "Alex"]
+print(users)
+
+users[1:3] = ["Robert", "JPJ"]
+print(users)
+
+users[4:5] = ["Elsa"]
+print(users)
+
+users.remove("Bob")
+print(users)
+
+print(users.pop())
+print(users)
+
+del users[0]
+print(users)
+
+# del data
+# print(data)
+
+data.clear()
+print(data)
+
+users[1:2] = ["dave"]
+users.sort()
+print(users)
+
+users.sort(key=str.lower)
+print(users)
+
+nums = [1, 5, 2, 4, 3]
+nums.reverse()
+print(nums)
+
+# nums.sort(reverse=True)
+# print(nums)
+
+print(sorted(nums, reverse=True))
+print(nums)
+
+numscopy = nums.copy()
+mynums = list(nums)
+mycopy = nums[:]
+
+print(numscopy)
+print(mynums)
+mycopy.sort()
+print(mycopy)
+print(nums)
+
+print(type(nums))
+
+
+mylist = list([1, "Neil", True])
+print(mylist)
+
+
+# Tuples
+mytuple = tuple(("Dave", 33, True))
+anothertuple = (1, 3, 4, 5, 5)
+
+print(mytuple)
+print(type(mytuple))
+print(type(anothertuple))
+
+newlist = list(mytuple)
+newlist.append("Neil")
+newtuple = tuple(newlist)
+print(newtuple)
+
+(one, two, *hey) = anothertuple
+print(one)
+print(two)
+print(hey)
+
+print(anothertuple.count(5))
